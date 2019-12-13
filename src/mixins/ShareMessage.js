f288fbd2615f7a03a8282833d1862a56c008715d
DZF <dengzhifeng_63@163.com>
2019-12-13 14:51:09 +0800

    分享

--- src/mixins/ShareMessage.js
+++ src/mixins/ShareMessage.js
@@ -34,13 +34,13 @@ export default class ShareMessage extends wepy.mixin {
     var url = currentPage.route    // 当前页面url
     var options = currentPage.options    // 如果要获取url中所带的参数可以查看options
     // 拼接url的参数
-    var urlWithArgs = url + '?from_openid=' + this.from_openid
-    for (var key in options) {
-      var value = options[key]
-      if (key !== 'from_openid') {
-        urlWithArgs = url + '?' + key + '=' + value + '&from_openid=' + this.from_openid
-      }
+    let parameter = ''
+    for (var item in options) {
+      parameter = `${parameter}${item}=${options[item]}&`
     }
+    var urlWithArgs = `${url}?${parameter}from_openid=${this.from_openid}`
+    console.log(parameter,'assoptions')
+    console.log(urlWithArgs)
     // urlWithArgs = urlWithArgs.substring(0, urlWithArgs.length - 1)
     return urlWithArgs
   }
